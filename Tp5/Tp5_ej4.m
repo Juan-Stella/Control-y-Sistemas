4c97574a7f302b69fc5e99c4472692eed84a6188
Juan-Stella <juanstella96@gmail.com>
2026-04-13 16:57:08 -0300

    .

--- Tp5/Tp5_ej4.m
+++ Tp5/Tp5_ej4.m
@@ -9,7 +9,8 @@ x = signal(:,1);   % canal 1
 
 Hd = filtro_pto4;  % crear el filtro
 
-x_filtered = filter(Hd, x);   % aplicar el filtro a la señal
+b = Hd.Numerator;
+x_filtered = filter(b, 1, x);
 
 [f_orig, mag_orig] = my_dft(x, fs);
 [f_filt, mag_filt] = my_dft(x_filtered, fs);
@@ -24,5 +25,27 @@ ylabel('Magnitud');
 title('Espectros de la señal original y filtrada');
 xlim([0 4000])
 
-sound(x, fs)
-sound(x_filtered, fs)
+%sound(x, fs)
+%sound(x_filtered, fs)
+
+% Comparación N=10, N=100 y N=1000
+Hd10 = Copy_of_filtro_pto4;  % crear el filtro
+hd1000 = Copy_2_of_filtro_pto4;
+b10   = Hd10.Numerator;
+b100  = Hd.Numerator;
+b1000 = hd1000.Numerator;
+
+[H10,   f] = freqz(b10,   1, 1024, 44100);
+[H100,  ~] = freqz(b100,  1, 1024, 44100);
+[H1000, ~] = freqz(b1000, 1, 1024, 44100);
+
+figure;
+plot(f, 20*log10(abs(H10)),   'r'); hold on;
+plot(f, 20*log10(abs(H100)),  'b');
+plot(f, 20*log10(abs(H1000)), 'g');
+legend('N=10', 'N=100', 'N=1000');
+grid on;
+xlabel('Frecuencia [Hz]');
+ylabel('Magnitud [dB]');
+title('Respuesta en frecuencia: N=10 vs N=100 vs N=1000');
+
